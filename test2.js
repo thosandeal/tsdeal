ebf2d473a8a1c872465111c240b3f7910ad92d43
thosandeal <thosandeal@gmail.com>
2024-08-28 01:45:54 +0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,15 +1,3 @@
-// function inputGoogleSheet(subId1 = '', subId2 = '', subId3 = '', subId4 = '', subId5 = '', subId6 = '') {
-//     page.goto('https://docs.google.com/forms/d/e/1FAIpQLScB82bM1zmvZ-h-jgENahEGk3GgfwmBicjfTZOOYJOwfn4QBw/viewform'); // vào google sheet điền thông tin
-//     page.waitForTimeout(1000);  // Đợi 3 giây
-//     page.waitForSelector('div[data-should-execute-invisible-captcha-challenge="false"] span span'); // đợi cho element xuất hiện thì thực hiện hành động tiếp theo
-//     if (subId1) pppinput('input[type="text"][autocomplete="off"][dir="auto"]', 1, subId1);
-//     if (subId2) pppinput('input[type="text"][autocomplete="off"][dir="auto"]', 2, subId2);
-//     if (subId3) pppinput('input[type="text"][autocomplete="off"][dir="auto"]', 3, subId3);
-//     if (subId4) pppinput('input[type="text"][autocomplete="off"][dir="auto"]', 4, subId4);
-//     if (subId5) pppinput('input[type="text"][autocomplete="off"][dir="auto"]', 5, subId5);
-//     if (subId6) pppinput('input[type="text"][autocomplete="off"][dir="auto"]', 6, subId6);
-// }
-
 function spinText(spinText) {
     // Thay thế \r\r thành \n
     var result = spinText.replace(/\r\r/g, '\n');
