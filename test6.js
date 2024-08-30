b860a2372bfba7f6a4b2915dcd98f0ce7aa91faf
thosandeal <thosandeal@gmail.com>
2024-08-30 14:58:12 +0700

    Update test6.js

--- test6.js
+++ test6.js
@@ -738,7 +738,7 @@ toiUuPage:async function (pcode, puid, pavatar, pbanner, pcity, pzip, pvitri, pp
     await page.waitForTimeout(5000);
 
     // tắt thông báo
-    await mfp.pcheckclick('div[aria-label="Dùng Trang"]',3000,"Tắt thông báo Dùng Trang nếu có");
+    await this.pcheckclick('div[aria-label="Dùng Trang"]',3000,"Tắt thông báo Dùng Trang nếu có");
     // **sửa địa chỉ pcity
     console.log("sửa địa chỉ");
     await this.pcheckclickx('//span[text()="Thêm địa chỉ của bạn"]', 1, 1000);
