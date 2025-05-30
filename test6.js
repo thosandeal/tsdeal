f79e357aa0af69650342d56ab8e501701df9b95a
thosandeal <thosandeal@gmail.com>
2025-05-30 15:13:29 +0700

    Update test6.js

--- test6.js
+++ test6.js
@@ -877,8 +877,6 @@ const mfp = {
             console.log('tiền hành edit mess');
             await this.pcheckclickx('//input[@aria-checked="false"][@aria-label="Đang tắt"]', 1, 4000);
             await this.pcheckclickx('//input[@aria-checked="false"][@aria-label="Đang tắt"]', 1, 4000);
-            await this.pcheckclickx('//input[@aria-checked="false"][@aria-label="Đang tắt"]', 1, 4000);
-
             // await this.pcheckclickx('//input[@aria-disabled="false"][@aria-label="Tắt"]', 1, 1000);
             await this.goVanBan('span[data-text="true"]', pmessok);
             await this.pcheckclickx('//div[text()="Lưu thay đổi"]', 1, 3000);
