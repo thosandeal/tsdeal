e1d9b1a975712d29726bb2b942d1cf24a083cd58
thosandeal <thosandeal@gmail.com>
2025-07-11 15:16:47 +0700

    Update test6.js

--- test6.js
+++ test6.js
@@ -347,6 +347,24 @@ const mfp = {
             await page.waitForTimeout(2000); // đợi 2s để hiển thị đề xuất
             await page.click('li[aria-selected="false"][role="option"]'); // chọn li đầu tiên làm vị trí
             await page.waitForTimeout(2000); // đợi 2s để xác nhận
+            // NHẬP THÊM 5 LẦN CHO MAX
+            const inputSelector = 'input[type="search"][role="combobox"][aria-label="Vị trí nhóm"]';
+
+if (await page.$(inputSelector)) {
+    for (let i = 1; i <= 5; i++) {
+        console.log(`Lần ${i}/5`);
+        
+        await page.click(inputSelector, { clickCount: 3 });
+        await page.keyboard.press('Backspace');
+        await page.type(inputSelector, pvitri);
+        await page.waitForTimeout(2000);
+        await page.click('li[aria-selected="false"][role="option"]');
+        
+        if (i < 5) await page.waitForTimeout(1000);
+    }
+}
+
+            // END : NHẬP THÊM 5 LẦN 
             await this.pcheckclickx('//span[text() = "Lưu"]', 2, 1000); // nhấn nút lưu vị trí
         }
         // sửa đối tượng tham gia nhóm
